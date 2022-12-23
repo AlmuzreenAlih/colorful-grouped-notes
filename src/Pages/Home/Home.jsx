2b6e1706cb8103622b1b83443757f0c1df0c531e
Almuzreen Alih <almuzreen.11@gmail.com>
2022-12-23 17:48:45 +0800

    Added conditions for displaying notes per group

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,53 +1,74 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { useRef} from 'react';
+import { useRef } from 'react';
 import React from 'react';
 import './Home.scss'
+
 function HomePage() {
-  const groups = [
-    {id: 1, title: 'Group 1', backgroundColor: 'red'},
-  ];
-  const notes = [
-    {id:1, title: 'Note 1', backgroundColor: 'blue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed quidem minima magnam laudantium ad qui alias vel voluptates iste, quod reprehenderit architecto ipsum quibusdam quisquam officiis omnis rem! Ab?'},
-    {id:2, title: 'Note 2', backgroundColor: 'blue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed quidem minima magnam laudantium ad qui alias vel voluptates iste, quod reprehenderit architecto ipsum quibusdam quisquam officiis omnis rem! Ab?'}
-  ];
+    ///////////////////////////////////// ITEMS //////////////////////////////////////////////////////////////
+    const groups = [
+        { id: 1, title: 'Group 1', backgroundColor: 'rgb(190,190,250)' },
+        { id: 2, title: 'Group 2', backgroundColor: 'rgb(190,250,190)' },
+        { id: 2, title: 'Group 3', backgroundColor: 'rgb(250,190,190)' },
+    ];
+    const notes = [
+        { id: 1, group: 'Group 1', title: 'Note 1', backgroundColor: 'blue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed quidem minima magnam laudantium ad qui alias vel voluptates iste, quod reprehenderit architecto ipsum quibusdam quisquam officiis omnis rem! Ab?' },
+        { id: 2, group: 'Group 1', title: 'Note 2', backgroundColor: 'blue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed quidem minima magnam laudantium ad qui alias vel voluptates iste, quod reprehenderit architecto ipsum quibusdam quisquam officiis omnis rem! Ab?' },
+        { id: 3, group: 'Group 2', title: 'Note 1A', backgroundColor: 'blue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed quidem minima magnam laudantium ad qui alias vel voluptates iste, quod reprehenderit architecto ipsum quibusdam quisquam officiis omnis rem! Ab?' },
+        { id: 4, group: 'Group 3', title: 'Note 1B', backgroundColor: 'blue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sed quidem minima magnam laudantium ad qui alias vel voluptates iste, quod reprehenderit architecto ipsum quibusdam quisquam officiis omnis rem! Ab?' },
+    ];
 
-  const textareaRef = useRef(null);
+    /////////////////////////////////// PARTIALS ////////////////////////////////////////////////////////////
+    //-------------------------------------------------------------------------------------------------------
+    function MainComponent() {
+        return (
+            <div className="AllNotes">
+            {groups.map(group => (
+                <NoteGroupComponent key={group.id} backgroundColor={group.backgroundColor} title={group.title} />
+                ))}
+            </div>
+        )
+    }
+    //--------------------------------------------------------------------------------------------------------
+    function NoteGroupComponent(props) {
+        return (
+            <div className="NoteGroup" style={{backgroundColor: props.backgroundColor}}>
+                <input value={props.title} />
+                {notes.filter((note) => note.group == props.title)
+                    .map(note => (
+                        <NoteComponent key={note.id} backgroundColor={note.backgroundColor} title={note.title} content={note.content} />
+                ))}
+            </div>
+        );
+    }
 
-  function adjustTextareaHeight() {
-    const textarea = textareaRef.current;
-    textarea.style.height = 'auto';
-    textarea.style.height = textarea.scrollHeight + 'px';
-  }
+    //--------------------------------------------------------------------------------------------------------
+    useEffect(() => {
+        const textareaElements = document.querySelectorAll('textarea');
+        textareaElements.forEach((textarea) => {
+            textarea.style.height = 'auto';
+            textarea.style.height = textarea.scrollHeight + 'px';
+        });
+    }, []);
 
-  function NoteComponent(props) {
+    function adjustTextareaHeight(e) {
+        e.target.style.height = 'auto';
+        e.target.style.height = e.target.scrollHeight + 'px';
+    }
+
+    function NoteComponent(props) {
+        return (
+            <div className='Note'>
+                <h2>{props.title}</h2>
+                <textarea onChange={adjustTextareaHeight}>{props.content}</textarea>
+            </div>
+        )
+    }
+
+    //////////////////////////////////////// MAIN RETURN ////////////////////////////////////////////////////////
     return (
-        <div className='Note'>
-            <h2>{props.title}</h2>
-            <textarea ref={textareaRef} onChange={adjustTextareaHeight}>{props.content}</textarea>
-            {/* <input value={props.content} /> */}
-        </div>
-    )
-  }
-
-  function GroupComponent(props) {
-    return (
-      <div className="NoteGroup" style={{}}>
-        <input value={props.title} />
-        {notes.map(note => (
-            <NoteComponent key={note.id} backgroundColor={note.backgroundColor} title={note.title} content={note.content} />
-        ))}
-      </div>
+        <MainComponent />
     );
-  }
-
-  return (
-    <div className="NoteGroups">
-      {groups.map(group => (
-        <GroupComponent key={group.id} backgroundColor={group.backgroundColor} title={group.title} />
-      ))}
-    </div>
-  );
 }
 
 export default HomePage;
